fb912d26804395231f3b66d81fe68f03942e5a90
Alan Sandoval <alansandoval47@aragon.unam.mx>
2023-02-07 00:02:38 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using Aspose.Cells;
+using ConsoleApp1;
+using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ArrayList petitionsWithStatus200 = new ArrayList();
+        ArrayList petitionsWithStatus400 = new ArrayList();
+        ArrayList petitionsWithStatus500 = new ArrayList();
+        StatusCodes statusCodes = new StatusCodes();
+        //string pathFile = "D:/Escritorio/proyectos/Ejemplo.log";
+        Console.WriteLine("Escriba la ruta absoluta del archivo log:");
+        string pathFile = Console.ReadLine()!;
+
+        Workbook workbook = new Workbook();
+        workbook.Worksheets.Add();
+        workbook.Worksheets.Add();
+        workbook.Worksheets[0].Name = "Status 200";
+        workbook.Worksheets[1].Name = "Status 400";
+        workbook.Worksheets[2].Name = "Status 500";
+
+
+        using (FileStream fs = File.Open(pathFile!, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+        using (BufferedStream bs = new BufferedStream(fs))
+        using (StreamReader sr = new StreamReader(bs))
+        {
+            string line;
+            while ((line = sr.ReadLine()!) != null)
+            {
+                if(Array.Exists(statusCodes.statusCodes200, statusCode => line.Contains(statusCode)))
+                {
+                    petitionsWithStatus200.Add(line.Substring(0, 8) + " " + line.Split("INFO")[1]);
+                    Console.WriteLine(line.Substring(0, 8) + " " + line.Split("INFO")[1]);
+                }
+
+                if (Array.Exists(statusCodes.statusCodes400, statusCode => line.Contains(statusCode)))
+                {
+                    petitionsWithStatus400.Add(line.Substring(0, 8) + " " + line.Split("INFO")[1]);
+                    Console.WriteLine(line.Substring(0, 8) + " " + line.Split("INFO")[1]);
+                }
+
+                if (Array.Exists(statusCodes.statusCodes500, statusCode => line.Contains(statusCode)))
+                {
+                    petitionsWithStatus500.Add(line.Substring(0, 8) + " " + line.Split("INFO")[1]);
+                    Console.WriteLine(line.Substring(0, 8) + " " + line.Split("INFO")[1]);
+                }
+
+            }
+        }
+
+        workbook.Worksheets[0].Cells.ImportArrayList(petitionsWithStatus200, 0, 0, true);
+        workbook.Worksheets[1].Cells.ImportArrayList(petitionsWithStatus400, 0, 0, true);
+        workbook.Worksheets[2].Cells.ImportArrayList(petitionsWithStatus500, 0, 0, true);
+
+        workbook.Save("data.xlsx");
+    }
+}
---
+++ ConsoleApp1/StatusCodes.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+namespace ConsoleApp1
+{
+    internal class StatusCodes
+    {
+        public String[] statusCodes200 = {
+            "Resultado: <200", "Resultado: <201", "Resultado: <202", "Resultado: <203"
+        };
+
+        public String[] statusCodes400 = {
+            "Resultado: <400", "Resultado: <401", "Resultado: <402", "Resultado: <403", "Resultado: <404"
+        };
+
+        public String[] statusCodes500 = {
+            "Resultado: <500", "Resultado: <501", "Resultado: <502", "Resultado: <503", "Resultado: <504"
+        };
+    }
+}
